3849ff6652f1ca7e2d8aafd8e700fbb241eadbf8
sdfgdsjhfg <115135161+sdfgdsjhfg@users.noreply.github.com>
2022-10-10 17:30:39 +0800

    repoke

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -41,10 +41,10 @@ func init() { // 插件主体
 		Handle(func(ctx *zero.Ctx) {
 			var nickname = zero.BotConfig.NickName[0]
 			switch {
-			case poke.Load(ctx.Event.GroupID).AcquireN(3):
+			case poke.Load(ctx.Event.GroupID).AcquireN(1):
 				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
-				ctx.SendChain(message.Text("请不要戳", nickname, " >_<"))
+				ctx.SendChain(message.Text("不要戳", nickname, " "))
 			case poke.Load(ctx.Event.GroupID).Acquire():
 				// 5分钟共8块命令牌 一次消耗1块命令牌
 				time.Sleep(time.Second * 1)
